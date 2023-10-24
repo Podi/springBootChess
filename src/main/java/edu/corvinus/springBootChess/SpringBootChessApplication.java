a8de23afd6606974b666da54fd4772d75ef98d43
podip <podipeti17@gmail.com>
2023-10-24 10:34:06 +0200

    Start

---
+++ src/main/java/edu/corvinus/springBootChess/SpringBootChessApplication.java
@@ -0,0 +1,13 @@
+package edu.corvinus.springBootChess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootChessApplication.class, args);
+	}
+
+}
